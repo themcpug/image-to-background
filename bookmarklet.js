d76868b4aae51dbd6b48b10f28209cc173377cc3
themcpug <119757607+themcpug@users.noreply.github.com>
2023-02-10 18:56:41 -0600

    Created bookmark file

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function()%7B%20%20%20var%20imageLink%20=%20prompt(%22Enter%20the%20image%20link:%22);%20%20%20document.body.style.backgroundImage%20=%20%22url(%22%20+%20imageLink%20+%20%22)%22;%20%7D)()
